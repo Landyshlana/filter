061aa75de15564fcfef5105d0af615536c5c20b8
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2024-09-26 14:50:39 +0300

    1

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import { ProjectList } from "../projectList/ProjectList.jsx";
+import { Toolbar } from "../toolbar/Toolbar";
+import cards from "../cards.js";
+
+export class Portfolio extends Component {
+      constructor(props) {
+      super(props);
+      this.state = {
+        selected: "All",
+        projects: cards}
+
+  onSelectFilter = (filter) => {
+    this.setState({ selected: filter });
+  };
+
+  getProjects = () => {
+    return this.state.selected === "All"
+      ? this.state.projects
+      : this.state.projects.filter(
+          (project) => project.category === this.state.selected
+        );
+  }
+  render(){
+      return (
+      <div className="container">
+        <Toolbar
+          filters={["All", "Websites", "Flayers", "Business Cards"]}
+          selected={this.state.selected}
+          onSelectFilter={this.onSelectFilter}
+        />
+        <ProjectList projects={this.getProjects()} />
+      </div>
+    );
+  }
+}
+}
